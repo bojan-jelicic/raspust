a05cfee850942d35dcb14b99963aa4177865a6e1
Bojan Jelicic <boki.jelicic91@gmail.com>
2017-12-22 16:36:46 +0100

    create new web app with hello module

---
+++ helloproject/hello/src/main/java/com/propelmedia/servlets/HelloServlet.java
@@ -0,0 +1,16 @@
+package com.propelmedia.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println("Hellooooo...");
+        out.close();
+    }
+}
